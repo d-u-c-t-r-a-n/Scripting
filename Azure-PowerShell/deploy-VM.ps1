b6e90547efc13f9c250914a96194305dce2fcdce
Duc Tran <56884438+d-u-c-t-r-a-n@users.noreply.github.com>
2025-02-15 20:02:19 -0500

    Add files via upload

--- Azure-PowerShell/deploy-VM.ps1
+++ Azure-PowerShell/deploy-VM.ps1
@@ -1,4 +1,19 @@
 # Step 1: Connect to Azure
 Connect-AzAccount -TenantId <TenantId>
 
-
+# Step 2: Define Variables
+$resourceGroup = "ResrcGroup"
+$location = "CanadaCentral"
+$vmName = "MyVM"
+$adminUsername = "admin"
+$adminPassword = "admin"
+$vnetName = "VNetName"
+$subnetName = "SubnetName"
+$publicIpName = "PublicIPName"
+$nsgName = "NSGName"
+$nicName = "NICName"
+$vmSize = "Standard_B2s"
+$imagePublisher = "MicrosoftWindowsServer"
+$imageOffer = "WindowsServer"
+$imageSku = "2022-datacenter"
+$osDiskName = "$vmName-OSDisk"
