5e9fb4cd91dc6d645d4322da97508b575033be82
Duc Tran <56884438+d-u-c-t-r-a-n@users.noreply.github.com>
2025-02-23 14:52:59 -0500

    Add files via upload

--- MS-Graph/add-group-member.ps1
+++ MS-Graph/add-group-member.ps1
@@ -2,9 +2,12 @@
 Get-ExecutionPolicy
 
 # Set policy
-Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass
+Set-ExecutionPolicy -Scope CurrentUser -ExecutionPolicy Bypass
 
 Install-Module -Name Microsoft.Graph
 
-# Connect to Microsoft Graph
+# Connect to MS Graph
 Connect-MgGraph
+
+# Get available MS Graph permissions in the tenant
+(Get-MgContext).Scopes | Format-List * 
