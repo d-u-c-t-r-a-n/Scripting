2df9b4742d2a28872312f8cfff22741adcd9fd38
Duc Tran <56884438+d-u-c-t-r-a-n@users.noreply.github.com>
2025-02-26 11:35:22 -0500

    final version

--- MS-Graph/add-group-member.ps1
+++ MS-Graph/add-group-member.ps1
@@ -2,20 +2,9 @@
 Get-ExecutionPolicy
 
 # Set policy
-Set-ExecutionPolicy -Scope CurrentUser -ExecutionPolicy Bypass
+Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass
 
 Install-Module -Name Microsoft.Graph
 
-# Connect to MS Graph
+# Connect to Microsoft Graph
 Connect-MgGraph
-
-# Get available MS Graph permissions in the tenant
-(Get-MgContext).Scopes | Format-List * 
-
-# Add member to a group 
-$groupId = "<groupId>"
-$userId = "<userId>"
-
-$params = @{
-"@odata.id" = "https://graph.microsoft.com/v1.0/users/$userId"
-}
